fae1d6b4c701333a30b15a76bfc4f0b22a1c5dea
Elayz <artemkartashyan@mail.ru>
2024-04-01 16:10:52 +0300

    middle panel T\L

--- src/components/MiddleSection/MiddleSection.js
+++ src/components/MiddleSection/MiddleSection.js
@@ -8,7 +8,8 @@ const MiddleSection = () => {
     const onChange = (a) => {
         console.log(a)
     }
-    const handleColor = '#ffffff'
+    const handleColor = '#ffffff';
+    const railStyleObj = { backgroundColor: '#69777d'};
     return (
         <div className={classes.blackSecondLayer}>
             <div className={classes.imageBlock}>
@@ -17,16 +18,47 @@ const MiddleSection = () => {
             <div className={classes.blackControlBlock}>
                 <div className={classes.blackLightAndTempBlock}>
                     <div className={classes.blackLight}>
-                        ОСВЕЩЕНИЕ
+                        <p className={classes.blackHeadtText}>ОСВЕЩЕНИЕ</p>
+                        <div className={classes.blackStatsValue}>
+                            00
+                            <div className={classes.blackStatsValueUnit}>%</div>
+                        </div>
                         <Slider
-                            trackStyle={{ backgroundColor: '#5fc5ab' }}
-                            handleStyle={{boxShadow: '0 0 0 4px #5fc5ab', borderRadius: '50px'}}
-
-                            defaultValue={30}  onChangeComplete={onChange} />
+                            trackStyle={{
+                                backgroundColor: '#5fc5ab',
+                            }}
+                            handleStyle={{
+                                boxShadow: '0 0 0 4px #5fc5ab',
+                                borderRadius: '50px',
+                            }}
+                            railStyle={railStyleObj}
+                            defaultValue={30}
+                            onChange={onChange}
+                        />
                     </div>
-                    <div className={classes.blackTemp}>ТЕМПЕРАТУРА</div>
+                    <div className={classes.blackTemp}>
+                        <p className={classes.blackHeadtText}>ТЕМПЕРАТУРА</p>
+                        <div className={classes.blackStatsValue}>
+                            00
+                            <div className={classes.blackStatsValueUnit}>°C</div>
+                        </div>
+                        <Slider
+                            trackStyle={{
+                                backgroundColor: '#5fc5ab',
+                            }}
+                            handleStyle={{
+                                boxShadow: '0 0 0 4px #5fc5ab',
+                                borderRadius: '50px',
+                            }}
+                            railStyle={railStyleObj}
+                            defaultValue={30}
+                            onChange={onChange}
+                        />
+                    </div>
                 </div>
-                <div className={classes.blackBlinds}>ШТОРЫ</div>
+                <div className={classes.blackBlinds}>
+                    <p className={classes.blackHeadtText}>ЖАЛЮЗИ</p>
+                </div>
             </div>
         </div>
     )
